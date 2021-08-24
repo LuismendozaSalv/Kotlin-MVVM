bc3cb22c6d5318aa44137d0d5b41b670955a1296
Luis Mendoza <luismendozasalv@gmail.com>
2021-08-24 06:46:45 -0400

    first commit

---
+++ app/src/main/java/com/mendosal/newsapp/ui/breaking/BreakingNewsFragment.kt
@@ -0,0 +1,28 @@
+package com.mendosal.newsapp.ui.breaking
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.mendosal.newsapp.R
+
+class BreakingNewsFragment : Fragment() {
+
+    private lateinit var homeViewModel: HomeViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        homeViewModel =
+                ViewModelProvider(this).get(HomeViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_breaking_news, container, false)
+
+        return root
+    }
+}
---
+++ app/src/main/java/com/mendosal/newsapp/ui/search/SearchNewsFragment.kt
@@ -0,0 +1,28 @@
+package com.mendosal.newsapp.ui.search
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.mendosal.newsapp.R
+
+class SearchNewsFragment : Fragment() {
+
+    private lateinit var notificationsViewModel: NotificationsViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        notificationsViewModel =
+                ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_search_news, container, false)
+
+        return root
+    }
+}
